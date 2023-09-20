852fea6e152624b6c738ea764b9ca99f8b97c917
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-09-20 14:09:56 +0430

    Add transaction to delete animal born after 2022

--- queries.sql
+++ queries.sql
@@ -44,4 +44,17 @@ select * from animals;
 
 rollback;
 
+select * from animals;
+
+begin;
+
+delete from animals
+where date_of_birth > '2022-01-01';
+
+savepoint sp1;
+
+update animals 
+set weight_kg = weight_kg * -1;
+
+commit;
 select * from animals
