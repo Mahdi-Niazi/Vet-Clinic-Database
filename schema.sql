e87a3623edfedd0736b7be46476f2771667e15e5
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-09-20 21:08:58 +0430

    Add owners and species table Update animal table

--- schema.sql
+++ schema.sql
@@ -11,3 +11,22 @@ CREATE TABLE animals (
 
 ALTER TABLE animals 
 ADD species varchar(50);
+
+create table owners(
+	id serial primary key,
+	full_name varchar(200),
+	age int
+);
+create table species(
+	id serial primary key,
+	name varchar(200)
+);
+
+alter table animals add primary key(id);
+alter table animals alter id add generated always as identity;
+alter table animals drop column species;
+alter table animals add column species_id int;
+alter table animals add constraint fk_species foreign key(species_id) references species(id);
+alter table animals add column owner_id int;
+alter table animals add constraint fk_owner foreign key(owner_id) references owners(id);
+select * from animals;
