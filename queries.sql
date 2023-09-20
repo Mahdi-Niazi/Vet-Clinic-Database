7fa9436012f7e78cdfe7da6cc3345db5f4d8dcb0
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-09-20 13:43:57 +0430

    Add transaction for animals species column.

--- queries.sql
+++ queries.sql
@@ -7,3 +7,15 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN TRANSACTION;
+
+UPDATE animals 
+SET species = 'unspecified';
+
+SELECT * FROM animals
+WHERE species = 'unspecified';
+
+Rollback;
+
+SELECT * FROM animals;
